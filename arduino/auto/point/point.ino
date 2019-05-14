b0edd515d63783cdb641b82a85b6bbb28e037151
Vilis <vilis.it@mail.com>
2019-05-14 16:59:05 +0300

    Pēdējās izmaiņas WeMos skriptā

--- arduino/auto/point/point.ino
+++ arduino/auto/point/point.ino
@@ -8,13 +8,13 @@
 #include <WiFiUdp.h>
 
 //***** Settings - UDP connection - START
-#define POINT_NAME                      "Red"           // the board name of the link
-#define STATION_NAME                    "MONORI"        // the name of the supervisor station
-#define WIFI_HOTSPOT_NAME               "bazargans"     // the WiFi hotspot name
-#define WIFI_HOTSPOT_PASS               "Mi0dirg8ss"    // the WiFi hotspot password
+#define POINT_NAME                      "green"           // the board name of the link
+#define STATION_NAME                    "raspisMelns"   // the name of the supervisor station
+//#define WIFI_HOTSPOT_NAME               "bazargans"     // the WiFi hotspot name
+//#define WIFI_HOTSPOT_PASS               "Mi0dirg8ss"    // the WiFi hotspot password
+#define WIFI_HOTSPOT_NAME               "BMSG2"         // the WiFi hotspot name
+#define WIFI_HOTSPOT_PASS               "barinu7755"    // the WiFi hotspot password
 #define UDP_PORT                        49750           // the predifined UDP port of the link
-//#define WIFI_HOTSPOT_NAME               "BMSG2"         // the WiFi hotspot name
-//#define WIFI_HOTSPOT_PASS               "barinu7755"    // the WiFi hotspot password
 //***** Settings - UDP connection - END
 
 //***** Settings - unified UDP messages - START
